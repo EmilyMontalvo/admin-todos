09a1373cfd207ad01a7d25ba819baf62d96b1d71
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-11 01:36:14 -0500

    feat: cookies server side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,6 +1,6 @@
 import { TabBar } from '@/components';
 import React from 'react'
-
+import { cookies } from 'next/headers';
 
 export const metadata = {
   title: 'Cookies Page',
@@ -8,12 +8,17 @@ export const metadata = {
 };
 
 const CookiesPage = () => {
+
+  const cookieStore = cookies(); //*Cookies Server side : Propio de Next
+  const cookieTab = Number(cookieStore.get('selectedTab')?.value??'1');
+
+
   return (
     <>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-3'>
         <div className='flex flex-col'>
           <span className='text-3xl'>Tabs</span>
-          <TabBar/>
+          <TabBar currentTab={cookieTab}/>
         </div>
 
       </div>
