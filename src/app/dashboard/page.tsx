ca47bd5f78ddf339492cc47d7d89c0b625ed7769
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-12 11:49:56 -0500

    feat: server side auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,35 @@
+import { auth } from '@/auth';
 import {WidgetItem} from '@/components/widgets/WidgetItem'
+import { redirect } from 'next/navigation';
 import React from 'react'
-
 
 export const metadata = {
  title: 'Dashboard',
  description: 'Dashboard',
 };
 
-const DashboardPage = () => {
+
+
+const DashboardPage = async () => {
+
+
+  const session = await auth()
+
+  if(!session){
+    redirect('/api/auth/signin');
+  }
+
   return (
     <>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <WidgetItem/>
-        <WidgetItem/>
-        <WidgetItem/>
+      <div className="grid gap-6 sm:grid-cols-2 grid-cols-1">
+        <WidgetItem title='Usuario conectado ServerSide'>
+        <div className='flex flex-col'>
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+          <span>{session.user?.email}</span>
+        </div>
+        </WidgetItem>
+         
       </div>
     </>
   )
