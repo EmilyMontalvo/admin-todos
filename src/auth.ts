ccbe09449f47e5a60ff12b4709dd088e7f58ae63
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-05-12 21:26:30 -0500

    feat: credentials provider

--- src/auth.ts
+++ src/auth.ts
@@ -3,29 +3,58 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import email from "next-auth/providers/email";
+import Credentials from "next-auth/providers/credentials"
+import { NextRequest } from 'next/server';
+import { signInEmailPasswor } from "./auth/actions/auth-actions";
+
 
 const prisma = new PrismaClient()
- 
+
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub, 
-    Google
+    GitHub,
+    Google,
+    Credentials({
+      credentials: {
+        email: { label: "Correo Electrónico", type: "email", placeholder: "usuario@google.com" },
+        password: { label: "Contraseña", type: "password", placeholder: "*********" },
+      },
+      async authorize(credentials, req)   {
+        //! Para login por API
+        // const response = await fetch(request) 
+        // if (!response.ok) return null
+        // return (await response.json()) ?? null
+        //!Para login manual
+        if (!('email' in credentials) || !('password' in credentials)) {
+          return null; // Si las credenciales no tienen email o password, retorna null
+        }
+
+        const user = await signInEmailPasswor(credentials!.email as string , credentials!.password as string);
+
+        if (!user) {
+        return null;
+        } 
+        
+        return user
+      },
+    }),
   ],
   session: {
-    strategy:'jwt'
-  }, 
-  callbacks:{
-    async signIn({user, account, profile, email, credentials}){
+    strategy: 'jwt'
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
       return true
 
     },
-    async jwt({ token, user, account, profile}) {
-      
-      const dbuser = await prisma.user.findUnique({where:{email:token.email?? 'no email'}})
+    async jwt({ token, user, account, profile }) {
 
-      if(dbuser?.isActive == false)throw Error('Usuario no activo')
+      const dbuser = await prisma.user.findUnique({ where: { email: token.email ?? 'no email' } })
+
+      if (dbuser?.isActive == false) throw Error('Usuario no activo')
 
 
 
@@ -37,8 +66,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
 
     async session({ session, user, token }) {
-      
-      if(session && session.user){
+
+      if (session && session.user) {
         session.user.roles = token.roles;
         session.user.id = token.id;
       }
